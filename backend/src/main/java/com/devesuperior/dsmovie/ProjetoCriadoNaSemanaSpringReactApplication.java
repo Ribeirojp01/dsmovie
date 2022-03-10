7fe783565dec658c159cd2362ba04157101e1596
Fernando Ribeiro <ribeirojp01@gmail.com>
2022-03-10 09:12:39 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devesuperior/dsmovie/ProjetoCriadoNaSemanaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCriadoNaSemanaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCriadoNaSemanaSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devesuperior/dsmovie/ProjetoCriadoNaSemanaSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devesuperior.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCriadoNaSemanaSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
